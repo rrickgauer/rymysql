1b542aca0df8f0419f3cf05a2be34b52a3f7b724
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-03-19 14:47:52 -0500

    update imports

--- src/pymysql/__init__.py
+++ src/pymysql/__init__.py
@@ -2,3 +2,7 @@ from . import commands as commands
 from . import credentials as credentials
 from . import connection as connection
 from . import structs as structs
+
+from .connection import *
+
+
